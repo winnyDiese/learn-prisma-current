78d97765f014b9aa0dad759ae9cd70766ce40826
winnyDiese <winnydiese@gmail.com>
2024-09-12 13:31:42 +0200

    lkjbkjhbuj

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,11 @@
 import express from "express"
 import {getPosts} from "./controllers/post"
 
+const app = express()
 
+app.get('/api/posts', getPosts)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
