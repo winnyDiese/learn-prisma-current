86bacb62bbc203ccfb360ac9bdfc85bc54638c86
winnyDiese <winnydiese@gmail.com>
2024-09-18 23:54:08 +0200

    lkjsbdflivub

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+const app = express();
+app.use(express.json());
+
+app.post('/api/posts', async (req,res)=>{
+    try {
+        const {title, body} = req.body
+        
+        if(!title || !body){
+            return res.status(400).json(({error:"Title and content are required."}))
+        }
+
+        const post = await prisma.post.create({
+            data:{
+                title,
+                body
+            }
+        })
+
+        res.status(201).json(post);
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error:'Internal server error'})
+    }
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
